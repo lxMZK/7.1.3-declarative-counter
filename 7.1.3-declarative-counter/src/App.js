f212f544e4abd3f62c079bf4f2718270badf9264
Alex Mizak <alexmmizak@gmail.com>
2022-08-08 19:17:17 -0400

    declarative counter

--- 7.1.3-declarative-counter/src/App.js
+++ 7.1.3-declarative-counter/src/App.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react'
 
-function App(){
+function App() {
   let [counter, changeCounter] = useState(1)
   window.changeCounter = changeCounter
   return (
-    <h1>{counter}</h1>
+    <div>
+      <h1>{counter}</h1>
+      <h4>{counter}</h4>
+    </div>
   )
 }
 
